d96a402999f9627142c2bfaedb63f04bf51d875c
hhh <1762851054@qq.com>
2022-02-15 21:30:34 +0800

    test

---
+++ sim_test.py
@@ -0,0 +1,19 @@
+import os
+
+local_filename = "./runs/detect/"
+# 遍历文件夹
+def walkFile(file):
+    for root, dirs, files in os.walk(file):
+
+        # root 表示当前正在访问的文件夹路径
+        # dirs 表示该文件夹下的子目录名list
+        # files 表示该文件夹下的文件list
+
+        # 遍历文件
+        for f in files:
+            print(os.path.join(root, f))
+
+        # 遍历所有的文件夹
+        for d in dirs:
+            print(os.path.join(root, d))
+walkFile(local_filename)
